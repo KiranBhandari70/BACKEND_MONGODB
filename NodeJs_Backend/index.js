828fd8df5843436b0197584eb85ccdb918c294ae
KiranBhandari70 <kiran.bhandariofficial70@gmail.com>
2025-09-21 16:28:28 +0545

    Calculator Demo practice

---
+++ NodeJs_Backend/index.js
@@ -0,0 +1,7 @@
+const calculator = require('./calculator');
+
+const add= calculator.add(5, 3);
+console.log("Addition: ",add);
+
+const subtract= calculator.subtract(5, 3);
+console.log("Subtraction: ",subtract);
